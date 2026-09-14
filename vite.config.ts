09e82b6a95598d7149dacfb512b8f0bb34fe45a2
dfoshams <98296145+dfoshams@users.noreply.github.com>
2026-09-14 17:55:57 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,9 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    plugins: [react(), tailwindcss()],
-    resolve: {
+  base: '/shiploop/',
+  plugins: [react(), tailwindcss()],
+  resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
       },
